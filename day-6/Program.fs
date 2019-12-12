463f245cc21ab83b5a34d5b63691ac551c6cf681
Andrew McGuier <andrew.mcguier@techelevator.com>
2019-12-12 17:57:23 -0500

    Day 6 Part 2

--- day-6/Program.fs
+++ day-6/Program.fs
@@ -16,7 +16,7 @@ type Node =
 
 let strToOrbit (str : string) =
   str.Split(')')
-  |> fun x -> (x.[0],x.[1])
+  |> fun x -> (x.[0].Trim(),x.[1].Trim())
   
 
 let rec orbitsToNode (orbits : Orbit list) idToProcess =
@@ -51,6 +51,34 @@ let validate orbits (node : Node) =
   |> List.filter (fun (x,y) -> not (set.Contains(x) && set.Contains(y)))
   |> List.isEmpty
 
+
+let inline (+.) (x1,y1) (x2,y2) = (x1+x2, y1+y2)
+
+let rec findYouSanta = function
+  | Leaf("SAN") -> (0,1)
+  | Leaf("YOU") -> (1,0)
+  | Leaf(_) -> (0,0)
+  | Node(id,lst) ->
+      let childResults =
+        lst
+        |> List.map findYouSanta
+        |> List.filter ((<>) (0,0))
+      match childResults.Length with
+        | 0 -> (0,0)
+        | 2 -> ((List.reduce (+.) childResults) : int*int)
+        | 1 ->
+            let (you,san) = childResults.Head
+            if you > 0 && san > 0 then
+              (you,san)
+            elif you > 0 then
+              (you + 1,san)
+            else
+              (you,san + 1)
+        | rs -> sprintf "Invalid state %A" rs |> failwith
+              
+            
+      
+
 [<EntryPoint>]
 let main argv =
   let orbits =
@@ -64,7 +92,11 @@ let main argv =
     |> fun l -> orbitsToNode l "COM"
 
 
-  printfn "%A" orbitTree
+  //printfn "%A" orbitTree
   countOrbits 0 orbitTree |> printfn "There are %i total orbits"
+
+  findYouSanta orbitTree
+  |> function |(x,y) -> x + y - 2
+  |> printfn "There are %i hops between you and Sanata"
   
   0 // return an integer exit code
